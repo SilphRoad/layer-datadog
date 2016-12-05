626af24f5687c3347f6247ba44c954c6b569fa0b
Marco Ceppi <marco@ceppi.net>
2016-12-05 15:39:44 -0500

    Dynamic endpoint loading

--- reactive/datadog.py
+++ reactive/datadog.py
@@ -6,13 +6,20 @@ from charms.reactive import (
      when_not,
      set_state,
      remove_state,
+     RelationBase,
 )
 
 from charmhelpers.core.host import service_restart
-from charmhelpers.core.hookenv import config
-from charmhelpers.core.templating import render
+from charmhelpers.core.hookenv import (
+    config,
+    metadata,
+)
 
+from charmhelpers.core.templating import render
 from charms import apt
+
+
+DATADOG_CONFD = '/etc/dd-agent/conf.d'
 
 
 @when_not('apt.installed.datadog-agent')
@@ -41,24 +48,57 @@ def reset_cfg():
     remove_state('datadog.configured')
 
 
-@when('php-fpm.available')
-@when('apt.installed.datadog-agent')
-@when_not('datadog.php-fpm.configured')
-def configure_php_fpm(php_fpm):
-    with open('/etc/dd-agent/conf.d/php_fpm.yaml.example') as f:
-        conf = yaml.safe_load(f.read())
+@when('datadog.configured')
+def configure_integrations():
+    """Configure all connected integrations currently available"""
+    # Use the metadata charm helper to load the metadata.yaml for datadog
+    # charm dynamically and parse requires relations
+    integrations = metadata().get('requires').keys()
 
-    inst = conf.get('instances', [])
-    inst[0] = {
-        'ping_url': php_fpm.ping_url,
-        'ping_reply': php_fpm.ping_reply,
-        'status_url': php_fpm.status_url,
-    }
+    # Loop over all the integrations defined in the charms metadata and
+    # check if they're available (ready to be configured)
+    for integration in integrations:
+        new_integration = False
+        example_file = os.path.join(DATADOG_CONFD,
+                                    '{}.yaml.example'.format(integration))
 
-    conf['instances'] = inst
+        # Make sure this integration exists and the relation is available
+        if not os.path.exists(example_file):
+            continue
 
-    with open('/etc/dd-agent/conf.d/php_fpm.yaml', 'w') as f:
-        f.write(yaml.dump(conf, default_flow_style=False))
+        if not is_state('{}.available'.format(integration)):
+            continue
 
-    service_restart('dd-agent')
-    set_state('datadog.php-fpm.configured')
+        # Load up the interfaces from reactive, extract the goodies and write
+        # out configuration files
+        rel = RelationBase.from_state(integration)
+        config = rel.configuration()
+
+        # Okay, we have configuration data, lets write out the file
+        integration_cfg = os.path.join(DATADOG_CONFD, '{}.yaml'.format(integration))
+        integration_file = integration_cfg
+
+        # This is the first time we've written this configuration file
+        # Load from the .example file for defaults, force restart
+        if not os.path.exists(integration_file):
+            integration_file = example_file
+            new_integration = True
+
+        # Load existing datadog configuration
+        with open(integration_file) as f:
+            cfg_file = yaml.safe_load(f.read())
+
+        inst = cfg_file.get('instances', [])
+        if inst[0] == config and not new_integration:
+            continue  # No changes to configuration, and not a new integration
+
+        cfg_file['instances'][0] = config
+
+        # Write configuration file
+        with open(integration_cfg, 'w') as f:
+            f.write(yaml.safe_dump(cfg_file, default_flow_style=False))
+
+        restart = True
+
+    if restart:
+        service_restart('dd-agent')
